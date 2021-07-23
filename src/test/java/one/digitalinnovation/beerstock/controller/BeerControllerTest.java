a750c224e75c273cfe72f80cebcee98676129e86
Akisimaru <44277751+Akisimaru@users.noreply.github.com>
2021-07-23 18:02:03 -0300

    v1.1.1 Beer Stocking Management
    optimized imports

--- src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -24,10 +24,7 @@ import java.util.Collections;
 
 import static one.digitalinnovation.beerstock.utils.JsonConvertionUtils.asJsonString;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.patch;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
